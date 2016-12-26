2c323fde2d871ab0dc2d2074032f607d53292749
Katrina Lee <katrinamarielee@gmail.com>
2016-12-26 18:37:11 +1100

    Prime Factors

---
+++ PrimeFactors.java
@@ -0,0 +1,29 @@
+public class PrimeFactors{
+  private int num;
+
+  public int GetPrimeFactors(int num) {
+    //assign the parameter to a number variable
+    int number = num;
+    //keep dividing the variable by the next number in ascending order starting with two until the variable can only be divisible by 1. Get it in ascending order!
+    int i=2;
+      while(number >1)
+        {
+          //When the variable is divisble
+          if(number % i == 0)
+          {
+            //Divide the variable by that number and reassign the variable to quotient
+            number = number/i;
+            //Print out the number that it's divisible by
+            System.out.print(i + ", ");
+          }
+          //if the number is not divisble, increment to the next possible number
+          else
+            i++;
+        }
+    return (i);
+  }
+  public static void main (String[] args) {
+    PrimeFactors number = new PrimeFactors();
+    number.GetPrimeFactors(30);
+  }
+}
