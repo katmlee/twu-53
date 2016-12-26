2d558da4baf86046cd327bf39219182a235aeba3
Katrina Lee <katrinamarielee@gmail.com>
2016-12-26 18:42:06 +1100

    updated alignment

--- PrimeFactors.java
+++ PrimeFactors.java
@@ -6,20 +6,18 @@ public class PrimeFactors{
     int number = num;
     //keep dividing the variable by the next number in ascending order starting with two until the variable can only be divisible by 1. Get it in ascending order!
     int i=2;
-      while(number >1)
-        {
-          //When the variable is divisble
-          if(number % i == 0)
-          {
-            //Divide the variable by that number and reassign the variable to quotient
-            number = number/i;
+      while(number >1) {
+        //When the variable is divisble
+        if (number % i == 0)
+          //Divide the variable by that number and reassign the variable to quotient
+          { number = number/i;
             //Print out the number that it's divisible by
             System.out.print(i + ", ");
-          }
           //if the number is not divisble, increment to the next possible number
-          else
-            i++;
+        } else {
+          i++;
         }
+      }
     return (i);
   }
   public static void main (String[] args) {
